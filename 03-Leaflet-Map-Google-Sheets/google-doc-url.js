d0c8904b5b551219309a66e656ded27b578d6d8c
Simon Renauld <simonrenauld.data@gmail.com>
2021-10-26 13:55:42 +0200

    updates

--- 03-Leaflet-Map-Google-Sheets/google-doc-url.js
+++ 03-Leaflet-Map-Google-Sheets/google-doc-url.js
@@ -2,4 +2,4 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/14er1kcawa8g3uHb9KGz-IB76x8jCmrlGu0BGyRGVmZw/edit#gid=1080875718';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyCnigEYPfUpvqn9zCVAd4vA_-Jt6ArGTuM';
+var googleApiKey = 'AIzaSyCnigEYPfUpvqn9zCVAd4vA_-Jt6ArGTuM';  f
