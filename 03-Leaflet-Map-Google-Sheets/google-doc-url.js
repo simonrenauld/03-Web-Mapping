b8ac2381e93bcafae91fbbd2dabcb0784fd744e5
Simon Renauld <79364202+simonrenauld@users.noreply.github.com>
2021-10-26 13:50:36 +0200

    Add files via upload

---
+++ 03-Leaflet-Map-Google-Sheets/google-doc-url.js
@@ -0,0 +1,5 @@
+// paste in your published Google Sheets URL from the browser address bar
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/14er1kcawa8g3uHb9KGz-IB76x8jCmrlGu0BGyRGVmZw/edit#gid=1080875718';
+
+// insert your own Google Sheets API key from https://console.developers.google.com
+var googleApiKey = 'AIzaSyCnigEYPfUpvqn9zCVAd4vA_-Jt6ArGTuM';
